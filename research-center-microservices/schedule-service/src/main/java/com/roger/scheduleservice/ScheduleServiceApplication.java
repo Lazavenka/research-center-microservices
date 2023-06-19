c9bab2a1b86d3b7aa8bd41931e37aec982b99434
Andrei Lazavenka <andrei_lazavenka@epam.com>
2023-06-19 14:24:14 +0200

    Config Eureka server-client

--- research-center-microservices/schedule-service/src/main/java/com/roger/scheduleservice/ScheduleServiceApplication.java
+++ research-center-microservices/schedule-service/src/main/java/com/roger/scheduleservice/ScheduleServiceApplication.java
@@ -2,7 +2,9 @@ package com.roger.scheduleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ScheduleServiceApplication {
     public static void main(String[] args) {
